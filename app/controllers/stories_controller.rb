ec447536263c2d9aa68f9cc14ac510f7a35c34bf
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-02 16:34:05 -0400

    stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,4 +3,19 @@ class StoriesController < ApplicationController
         stories = Story.all
         render json: stories.to_json
     end 
+
+    def new 
+       story = Story.new 
+    end 
+
+    def create 
+        story = Story.create(story_params)
+        render json: story.to_json 
+    end 
+
+    private 
+
+    def story_params 
+        params.require(:story).permit(:user_first_name, :user_last_name, :story_title, :sentance_1)
+    end 
 end
